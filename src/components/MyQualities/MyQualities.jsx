b2140ab62f3136983c160bc995bfecf11e6ed36f
ashikdas0002 <ashikdas0002@gmail.com>
2024-09-25 17:16:30 +0600

    quality section

--- src/components/MyQualities/MyQualities.jsx
+++ src/components/MyQualities/MyQualities.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './MyQualities.css'
+import MyQuality from '../MyQuality/MyQuality';
 const MyQualities = () => {
     return (
         <div className='mt-48'>
@@ -8,9 +9,12 @@ const MyQualities = () => {
                 <h1 className='text-5xl text-center font-bold intro-text'>
                     My Quality Services
                 </h1>
-                <p className='text-1xl text-info text-center mt-4 font-bold'>
+                <p className='text-[23px] text-info text-center mt-4 font-bold'>
                     We put your ideas and thus your wishes in the form of a unique web project that <br /> inspires you and you customers.
                 </p>
+            </div>
+            <div>
+                <MyQuality />
             </div>
         </div>
     );
---
+++ src/components/MyQuality/MyQuality.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './MyQuality.css'
+
+const MyQuality = () => {
+    return (
+        <div>
+            {/* qualites  */}
+            <div className='flex'>
+                <div className='flex'>
+                    <h2 className='text-[#8750f7]'>01</h2>
+                    <h3 className='text-[#FFFFFF]'>Front End WeB Dev</h3>
+                </div>
+                <div className='text-[#FFFFFF]'>
+                    Get More Experience with AD WEB DEV
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default MyQuality;
